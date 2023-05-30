9989440deda7f089762a9d0179a9f8d67143308b
Greg Doud <gpdoud@gmail.com>
2023-05-30 10:01:26 -0400

    Added properties to CustomerMaster model

---
+++ Migrations/20230528224659_enhanced customer master.cs
@@ -0,0 +1,302 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace career_api_server.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnhancedCustomerMaster : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CompanyMasters_Code",
+                table: "CompanyMasters");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Users",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Lastname",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Firstname",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyName",
+                table: "CompanyMasters",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "CompanyMasters",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "BlendedRoles",
+                table: "CompanyMasters",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CodingRoles",
+                table: "CompanyMasters",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactRole",
+                table: "CompanyMasters",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "CompanyMasters",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ItbaRoles",
+                table: "CompanyMasters",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "CompanyMasters",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "SecurityRoles",
+                table: "CompanyMasters",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "VirtualRoles",
+                table: "CompanyMasters",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyMasters_Code",
+                table: "CompanyMasters",
+                column: "Code",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CompanyMasters_Code",
+                table: "CompanyMasters");
+
+            migrationBuilder.DropColumn(
+                name: "BlendedRoles",
+                table: "CompanyMasters");
+
+            migrationBuilder.DropColumn(
+                name: "CodingRoles",
+                table: "CompanyMasters");
+
+            migrationBuilder.DropColumn(
+                name: "ContactRole",
+                table: "CompanyMasters");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "CompanyMasters");
+
+            migrationBuilder.DropColumn(
+                name: "ItbaRoles",
+                table: "CompanyMasters");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "CompanyMasters");
+
+            migrationBuilder.DropColumn(
+                name: "SecurityRoles",
+                table: "CompanyMasters");
+
+            migrationBuilder.DropColumn(
+                name: "VirtualRoles",
+                table: "CompanyMasters");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Users",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Lastname",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Firstname",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyName",
+                table: "CompanyMasters",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "CompanyMasters",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyMasters_Code",
+                table: "CompanyMasters",
+                column: "Code",
+                unique: true,
+                filter: "[Code] IS NOT NULL");
+        }
+    }
+}
--- Models/CompanyMaster.cs
+++ Models/CompanyMaster.cs
@@ -14,18 +14,29 @@ public class CompanyMaster {
     [StringLength(50)]
     public string CompanyName { get; set; } = string.Empty;
     [StringLength(50)]
-    public string Address { get; set; } = string.Empty;
+    public string? Address { get; set; } = null;
     [StringLength(50)]
-    public string City { get; set; } = string.Empty;
+    public string? City { get; set; } = null;
     [StringLength(2)]
-    public string StateCode { get; set; } = string.Empty;
+    public string? StateCode { get; set; } = null;
     [StringLength(10)]
-    public string Zip { get; set; } = string.Empty;
+    public string? Zip { get; set; } = null;
+    [StringLength(12)]
+    public string? Phone { get; set; } = null;
+    [StringLength(100)]
+    public string? Email { get; set; } = null;
     [StringLength(50)]
-    public string ContactName { get; set; } = string.Empty;
+    public string? ContactName { get; set; } = null;
     [StringLength(20)]
-    public string ContactPhone { get; set; } = string.Empty;
-    
+    public string? ContactPhone { get; set; } = null;
+    [StringLength(50)]
+    public string? ContactRole { get; set; } = null;
+    public bool CodingRoles { get; set; } = false;
+    public bool ItbaRoles { get; set; } = false;
+    public bool SecurityRoles { get; set; } = false;
+    public bool BlendedRoles { get; set; } = false;
+    public bool VirtualRoles { get; set; } = false;
+
     public bool Active { get; set; } = true;
     public DateTime Created { get; set; } = DateTime.Now;
     public DateTime? Updated { get; set; } = null;
